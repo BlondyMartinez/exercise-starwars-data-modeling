92e4c25f4aa8a060fadf9b80dac2ac4e101f0e3c
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-04-26 20:34:02 +0000

    star wars database model

--- src/models.py
+++ src/models.py
@@ -7,26 +7,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(10))
+    diameter = Column(String(10))
+    rotation_period = Column(String(5))
+    orbital_period = Column(String(10))
+    gravity = Column(String(10))
+    population = Column(String(20))
+    climate = Column(String(20))
+    terrain = Column(String(20))
+    surface_water = Column(String(4))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    full_name = Column(String)
+    hair_color = Column(String(10))
+    eye_color = Column(String(10))
+    skin_color = Column(String(10))
+    height = Column(String(3))
+    weight = Column(String(20)) 
+    gender = Column(String(10))
+    year_of_birth = Column(String(10))
+    homeworld_id = Column(Integer, ForeignKey('homeworld.id'))
+    homeworld = relationship(Planet)
 
-    def to_dict(self):
-        return {}
+class FavoritePlanets(Base):
+    __tablename__ = 'favorite_planets'
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+    
+class FavoriteCharacters(Base):
+    __tablename__ = 'favorite_characters'
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30))
+    password = Column(String(30))
+    favorite_planets_id = Column(Integer, ForeignKey('favorite_planets.id'))
+    favorite_planets = relationship(FavoritePlanets)
+    favorite_characters_id = Column(Integer, ForeignKey('favorite_characters.id'))
+    favorite_characters = relationship(FavoriteCharacters)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
